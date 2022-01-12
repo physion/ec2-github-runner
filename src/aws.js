2fc09cd9cc0ed10ec530b14a854a37c9bf7a23da
= <ryan@ovation.io>
2022-01-12 15:24:04 -0800

    Adding env var to profile

--- src/aws.js
+++ src/aws.js
@@ -14,7 +14,9 @@ async function startEc2Instance(label, githubRegistrationToken) {
     'curl -O -L https://github.com/actions/runner/releases/download/v2.278.0/actions-runner-linux-${RUNNER_ARCH}-2.278.0.tar.gz',
     'tar xzf ./actions-runner-linux-${RUNNER_ARCH}-2.278.0.tar.gz',
     'export RUNNER_ALLOW_RUNASROOT=1',
+    'sudo echo "export RUNNER_ALLOW_RUNASROOT=1" >> /etc/profile.d/env.sh',
     'export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1',
+    'sudo echo "export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1" >> /etc/profile.d/env.sh',
     `./config.sh --unattended --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
     './run.sh',
   ];
